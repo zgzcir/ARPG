a4c96101968115d7343da431f65ff154181df095
zgzcir <zgzcir@outlook.com>
2019-08-05 14:07:18 +0800

    战斗数据接口

--- Assets/Scripts/Systems/BattleSystem/BattleSys.cs
+++ Assets/Scripts/Systems/BattleSystem/BattleSys.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class BattleSys : BaseSystem
 {
-
-    
     public static BattleSys Instance;
+    public BattlePanel BattlePanel;
+
+    public BattleManager BattleManager;
 
     public override void InitSys()
     {
@@ -17,14 +18,29 @@ public class BattleSys : BaseSystem
 
     public void EntoBattle(int mapID)
     {
-        GameObject go=new GameObject()
+        GameObject go = new GameObject()
         {
             name = "BattleRoot"
         };
         go.transform.SetParent(GameRoot.Instance.transform);
 
-        BattleManager battleManager = go.AddComponent<BattleManager>();
-        battleManager.InitManager(mapID);
+        BattleManager = go.AddComponent<BattleManager>();
+        BattleManager.InitManager(mapID);
+        SetBattlePanelState();
     }
 
+    public void SetBattlePanelState(bool isActive = true)
+    {
+        BattlePanel.SetPanelState(isActive);
+    }
+
+    public void SetSelfPlayerMoveMobileDir(Vector2 dir)
+    {
+        BattleManager.SetSelfPlayerMoveMobileDir(dir);
+    }
+
+    public void ReqReleaseSkill(int index)
+    {
+        BattleManager.ReqReleaseSkill(index);
+    }
 }
